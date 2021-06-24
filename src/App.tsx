dc54cba4e30135a65f39be2070d2c4cfb87b80db
ladeia <252842.5@gmail.com>
2021-06-24 12:43:12 -0300

    refatorar

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,16 @@
-import { createContext, useState, useEffect } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
-import { auth, firebase } from './services/firebase'
-
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoon'
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-export const AuthContext = createContext({} as AuthContextType)
+import { AuthContextProvider } from './context/AuthContext'
 
 function App() {
-  const [user, setUser] = useState<User>()
-
-  useEffect(() => {
-    auth.onAuthStateChanged(user => {
-      if(user) {
-        const { displayName, photoURL, uid } = user
-
-        if(!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.')
-        }
-  
-        setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-        })
-      }
-    })
-  }, [])
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    const result = await auth.signInWithPopup(provider)
-
-    if(result.user) {
-      const { displayName, photoURL, uid } = result.user
-
-      if(!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account.')
-      }
-
-      setUser({
-      id: uid,
-      name: displayName,
-      avatar: photoURL
-      })
-    }
-  }
-  
-
+ 
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+     <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/room/new" component={NewRoom} />
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,14 @@
-import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
-
-import { AuthContext } from '../App'
-
+import { useAuth } from '../hooks/useAuths'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
-
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 export function Home() {
     const history = useHistory()
-    const { user, signInWithGoogle } = useContext(AuthContext)
+    const { user, signInWithGoogle } = useAuth()
 
     async function handleCreateRoom() {
         if(!user) {
